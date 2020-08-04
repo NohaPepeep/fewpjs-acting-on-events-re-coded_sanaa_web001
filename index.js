4725adb8763d37f4dd87be1d750295916e50cdd6
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-04 12:38:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ function moveDodgerLeft() {
   }
 }
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
+  let rightNumbers = dodger.style.left.replace("px", "");
   let right = parseInt(rightNumbers, 10);
  
   if (right < 400) {
-    dodger.style.right = `${right + 1}px`;
+    dodger.style.left = `${right + 1}px`;
   }
 }
 document.addEventListener("keydown", function(e) {
