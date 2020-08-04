9f17ee733b503c84f17893d78b81692a0ad80ce3
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-08-04 12:36:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function moveDodgerRight() {
   let right = parseInt(rightNumbers, 10);
  
   if (right < 400) {
-    dodger.style.right = `${right - 1}px`;
+    dodger.style.right = `${right + 1}px`;
   }
 }
 document.addEventListener("keydown", function(e) {
